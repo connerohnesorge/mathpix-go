18310f9bd62c8d9bb2498a6182921c08a2db49ce
connerohnesorge <88785126+connerohnesorge@users.noreply.github.com>
2025-12-31 17:24:17 -0600

    Update package description for mathpix-go API

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package api contains the main entry point for the pegwings.ai API.
+// Package api contains the main entry point for the mathpix-go API.
 package mathpix
 
 //go:generate gomarkdoc -o README.md -e .
